fc50c9353660fe80787740a6462dce1b6b45ab23
UK <hwangwonuk94@gmail.com>
2024-08-23 07:54:15 +0900

    init

---
+++ src/main/java/com/example/springbasicmvc/SpringBasicMvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbasicmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBasicMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbasicmvc/SpringBasicMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbasicmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
